c96741cc7ebb620c7f9495ac1793e242ce365e5c
shin12123 <bludjust@gmail.com>
2024-03-20 09:44:14 +0200

    update

--- LABA-3/Program.cs
+++ LABA-3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 
+namespace LABA_3
 //1) Створити клас з двома змінними. 
 //Додати конструктор з вхідними параметрами. 
 //Додати конструктор, який ініціалізує члени класу за замовчуванням. Додати деструктор, що виводить на екран повідомлення про видалення об’єкту.
@@ -7,66 +8,67 @@
 //Метод 1. Зберігає створений об’єкт класу з Завдання 1 у JSON файл 
 //Метод 2. Відкриває JSON файл з даними та створює об’єкт класу з цими даними для виконання Завдання 1.
 
-
-public class Class
 {
-   
-    public int Number { get; set; }
-    public string Text { get; set; }
-
-
-
-    public Class(int number, string text)
+    public class cl
     {
-        
-        Number = number;
-        Text = text;
+        // Класс с 2 переменными
+        public int Number { get; set; }
+        public string Text { get; set; }
 
+
+        //Додати конструктор з вхідними параметрами
+        public cl(int number, string text)
+        {
+
+            Number = number;
+            Text = text;
+
+        }
+
+        //Додати конструктор, який ініціалізує члени класу за замовчуванням
+        public cl() : this(0, "Просто текст")
+        {
+        }
+
+        //Додати деструктор, що виводить на екран повідомлення про видалення об’єкту.
+        ~cl()
+        {
+
+            Console.WriteLine("Объект Class удаляется");
+
+        }
+
+        // Сериализация в json
+        public static void SerializeToJson(cl obj, string filePath)
+        {
+
+            string jsonString = JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        // Десериализация файла и возвращение в объект Class
+        public static cl DeserializeFromJson(string filePath)
+        {
+
+            string jsonString = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<cl>(jsonString);
+
+        }
     }
 
 
-    public Class() : this(0, "Просто текст")
-    {
-    }
-
-
-    ~Class()
+    class Program
     {
 
-        Console.WriteLine("Объект MyClass удаляется");
+        static void Main()
+        {
 
-    }
+            cl o1 = new cl(123, "Пример");
+            cl.SerializeToJson(o1, @"C:\Users\bludj\source\repos\LABA-3\1.json");
+            cl deserializedObject = cl.DeserializeFromJson("1.json");
 
-
-    public static void SerializeToJson(Class obj, string filePath)
-    {
-       
-        string jsonString = JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
-    
-        File.WriteAllText(filePath, jsonString);
-    }
-
- 
-    public static Class DeserializeFromJson(string filePath)
-    {
-      
-        string jsonString = File.ReadAllText(filePath);
-        return JsonSerializer.Deserialize<Class>(jsonString);
-
-    }
-}
-
-
-class Program
-{
-  
-    static void Main()
-    {
-      
-        Class m1 = new Class(123, "Пример");
-        Class.SerializeToJson(m1, @"C:\Users\bludj\source\repos\LABA-3\1.json");
-        Class deserializedObject = Class.DeserializeFromJson("1.json");
-
-        Console.WriteLine($"Number: {deserializedObject.Number}, Text: {deserializedObject.Text}");
+            Console.WriteLine($"Number: {deserializedObject.Number}, Text: {deserializedObject.Text}");
+        }
     }
 }
